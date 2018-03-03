70e79175687fac9cb4e17c40fbe42a2c0c4e8309
Michael-BCM <michael@bluecarbonmedia.co.uk>
2018-03-03 08:23:17 +0000

    Video 5 complete

---
+++ Brackeys Multiplayer FPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+
+    private float mouseSensitivity = 3;
+
+    [SerializeField]
+    private float speed = 5F;
+    
+    [SerializeField]
+    private float thrusterForce = 1000F;
+
+    [Header("Spring Settings:")]
+    [SerializeField]
+    private JointDriveMode jointMode = JointDriveMode.Position;
+
+    [SerializeField]
+    private float jointSpring = 20F;
+
+    [SerializeField]
+    private float jointMaxForce = 40F;
+
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        SetJointSettings(jointSpring);
+    }
+
+    private void Update()
+    {
+        //calculate movement velocity as a 3D vector.
+
+        //'GetAxis' as standard applies a smoothing filter. 'GetAxisRaw' doesn't.
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        //Final movement vector
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        //Apply movement
+        motor.Move(velocity);
+
+        //Calculate rotation as a 3D vector (turning around);
+        float yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0F, yRotation, 0F) * mouseSensitivity;
+
+        //Apply rotation
+        motor.Rotate(rotation);
+
+        //Calculate camera rotation as a 3D vector (looking up and down);
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+
+        float cameraRotationX = xRotation * mouseSensitivity;
+
+        //Apply rotation
+        motor.RotateCamera(cameraRotationX);
+
+        Vector3 _thrusterForce = Vector3.zero;
+
+        //Calculate thruster force based on player input. 
+        if(Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+            SetJointSettings(0F);
+        }
+        else
+        {
+            SetJointSettings(jointSpring);
+        }
+
+        //Apply thruster force
+        motor.ApplyThruster(_thrusterForce);
+
+    }
+
+    private void SetJointSettings (float _jointSpring)
+    {
+        //Notice how the 'new JointDrive' doesn't have regular brackets before the curly braces?
+        //This is because it's possible to initialise individual values of a 'new' object without the use of a constructor,
+        //or ever needing to create one. 
+        joint.yDrive = new JointDrive
+        {
+            mode = jointMode, positionSpring = jointSpring, maximumForce = jointMaxForce
+        };
+    }
+}
---
+++ Brackeys Multiplayer FPS/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Vector3 rotation = Vector3.zero;
+
+    private float cameraRotationX = 0;
+
+    private float currentCameraRotationX = 0;
+
+    private Rigidbody _rigidbody;
+
+    private Vector3 thrusterForce = Vector3.zero;
+
+    [SerializeField]
+    private float cameraRotationLimit = 89.9F;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    //Gets a movement vector.
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Gets a rotational vector.
+    public void Rotate (Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    //Gets a rotational vector for the camera.
+    public void RotateCamera(float _cameraRotation)
+    {
+        cameraRotationX = _cameraRotation;
+    }
+
+    //Get a force vector for our thruster. 
+    public void ApplyThruster(Vector3 _thrusterForce)
+    {
+        thrusterForce = _thrusterForce;
+    }
+
+    //Runs every physics iteration.
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Perform movement based on velocity variable.
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            _rigidbody.MovePosition(_rigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if(thrusterForce != Vector3.zero)
+        {
+            _rigidbody.AddForce(thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    //Perform rotation
+    private void PerformRotation()
+    {
+        _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(rotation));
+        if(cam != null)
+        {
+            //Set our rotation and clamp it.
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            //Apply our rotation to the transform of the camera. 
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0, 0);
+        }
+    }
+}
